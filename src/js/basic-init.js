eacc0dd87e674be08b914435550f61f53e03debb
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-12-23 12:13:54 +0200

    Завершил верстку страницы проекта

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -5,6 +5,33 @@ $(document).ready(function () {
     const is_mobile = isMobile();
     if (is_mobile) document.documentElement.classList.add('is-mobile');
 
+    // sticky menu
+    let mainHeader = document.querySelector('.main-header');
+    let mainHeaderWrapper = document.querySelector('.main-header__wr');
+    let waypointOffset = 50;
+
+    if (mainHeaderWrapper) {
+        let waypoint = new Waypoint({
+            element: mainHeader,
+            handler: function (direction) {
+                if (direction === 'down') {
+                    mainHeader.style.height = mainHeaderWrapper.offsetHeight + 'px';
+                    mainHeaderWrapper.classList.add('main-header__wr--sticky');
+                    mainHeaderWrapper.classList.remove('main-header__wr--end-sticky');
+                    mainHeaderWrapper.style.top = -mainHeaderWrapper.outerHTML + 'px';
+                } else {
+                    waypointOffset = 0;
+                    mainHeader.style.height = 'auto';
+                    mainHeaderWrapper.classList.remove('main-header__wr--sticky');
+                    mainHeaderWrapper.classList.add('main-header__wr--end-sticky');
+                }
+            },
+            offset: function() {
+                return -(this.element.clientHeight + waypointOffset);
+            }
+        });
+    }
+
     // parallax
     if (!is_mobile) {
         let scene = document.querySelectorAll('.js-scene');
@@ -13,8 +40,15 @@ $(document).ready(function () {
         });
     }
 
+    // init tabs
+    $('.authors-tabs').tabs();
+    $('.tabs-steps').tabs();
+
     // masked input
     $('input[type="tel"]').mask('+7 (999) 999-99-99 ');
+
+    // get the width of the scrollbar
+    document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
 
     // is mobile
     function isMobile() {
